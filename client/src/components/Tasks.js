e6262d1920d94a30666915fd4445b14dd26f3a27
setentaysiete.anonimo@gmail.com <setentaysiete.anonimo@gmail.com>
2020-11-29 20:56:43 -0300

    second commit

--- client/src/components/Tasks.js
+++ client/src/components/Tasks.js
@@ -8,6 +8,9 @@ export default class Tasks extends Component {
         super(props);
     }
 
+
+    
+
     state = {
         name: '',
         description: '',
@@ -23,6 +26,44 @@ export default class Tasks extends Component {
         
     }
 
+    handleInputChange(event) {
+        console.log(event.target.name);
+        if (event.target.name === "name") {
+            this.setState({name: event.target.value});
+        } else if (event.target.name === "description") {
+            this.setState({description: event.target.value});
+        }
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        let task = {
+            name: this.state.name,
+            description: this.state.description
+        };
+        axios.post('http://localhost:5000/api/tasks', task).then((res) => {
+            let data = res.data;
+            this.setState({ tasks: data });
+            this.setState({ name: '',description: '' })
+        }).catch(err => console.error(err));
+    }
+
+    handleDeleteClick(event, id) {
+        axios.delete(`http://localhost:5000/api/tasks/${id}`).then((res) => {
+            let data = res.data;
+            this.setState({ tasks: data });
+            this.setState({ name: '',description: '' })
+        }).catch(err => console.error(err));
+    }
+
+    handleUpdate(event, id) {
+        axios.put(`http://localhost:5000/api/tasks/${id}`, {}).then((res) => {
+            let data = res.data;
+            this.setState({ tasks: data });
+            this.setState({ name: '',description: '' })
+        }).catch(err => console.error(err));
+    }
+
     render() {
         
         let tasks = []
@@ -30,14 +71,50 @@ export default class Tasks extends Component {
         for (let i = 0; i<this.state.tasks.length;i++) {
             tasks.push(
                 
-                <li key={this.state.tasks[i]._id}>{this.state.tasks[i].name}</li>
+                <tr key={this.state.tasks[i]._id}>
+                    <td>{i}</td>
+                    <td>{this.state.tasks[i].name}</td>
+                    <td>{this.state.tasks[i].description}</td>
+                    <td><input className="form-check-input" type="checkbox" name="checkbox" checked={this.state.tasks[i].done} onChange={(event) => this.handleUpdate(event, this.state.tasks[i]._id)} /></td>
+                    <td><button className="btn-danger text-white" onClick={(event) => this.handleDeleteClick(event, this.state.tasks[i]._id)}><i className="far fa-trash-alt"></i></button></td>
+
+                </tr>
                 
             )
         }
         return (
-            <ul>
+            <div>
+                <table className="table" style={{ width: "70%" }}>
+                <thead>
+
+                <tr>
+                    <th>Id</th>
+                    <th>Name</th>
+                    <th>Description</th>
+                    <th>Done</th>
+                    <th>Actions</th>
+                </tr>
+                </thead>
+                <tbody>
                 {tasks}
-            </ul>
+                </tbody>
+            </table>
+            <br/>
+            <br/>
+            <div className="card" style={{width: "70%"}}>
+                <form className="card-body" onSubmit={(event) => this.handleSubmit(event)}>
+                    <div className="form-group">
+                        <label htmlFor="name">Name</label>
+                        <input type="text" name="name" value={this.state.name} onChange={(event) => this.handleInputChange(event)} className="form-control" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="description">Description</label>
+                        <input type="text" name="description" value={this.state.description} onChange={(event) => this.handleInputChange(event)} className="form-control" />
+                    </div>
+                    <button type="submit" className="btn btn-primary">Add</button>
+                </form>
+            </div>
+            </div>
         )
     }
 
